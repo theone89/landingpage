87dfb9fc2112b12e683d21cf4a2dbea210b5215e
ThEOnE89 <franklincampos8907@gmail.com>
2025-02-01 16:36:15 -0500

    update roboto

--- src/app/robotos.ts
+++ src/app/robotos.ts
@@ -3,30 +3,10 @@ import type { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
-      // Regla para Googlebot
-      {
-        userAgent: "Googlebot",
-        allow: ["/"],
-        disallow: ["/private/", "/admin/"],
-        crawlDelay: 10,
-      },
       // Regla para todos los bots (userAgent: *)
       {
         userAgent: "*",
-        allow: ["/public/", "/blog/"],
-        disallow: ["/private/", "/admin/"],
-      },
-      // Regla para Bingbot
-      {
-        userAgent: "Bingbot",
         allow: ["/"],
-        disallow: ["/private/"],
-      },
-      // Regla para Applebot
-      {
-        userAgent: "Applebot",
-        allow: ["/"],
-        disallow: ["/private/"],
       },
     ],
     sitemap: "https://strongfreecode.com/sitemap.xml",
