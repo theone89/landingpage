3075262c086b508d4078b563ad08e7a285f54dda
ThEOnE89 <franklincampos8907@gmail.com>
2025-02-02 16:02:38 -0500

    update

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,7 +1,22 @@
-export default function BlogLayout({
+import { fetchData } from "@/lib/fetchUtils";
+import Aside from "./components/Aside";
+import { Article } from "./types/article";
+
+export default async function BlogLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <section className="relative py-24 ">{children}</section>;
+  const articles: Article[] = await fetchData("/api/blog/articles");
+  return (
+    <section className="relative py-24 flex flex-wrap">
+      {/* Artículos en la parte principal */}
+      <div className="md:w-2/3 pr-4">{children}</div>
+
+      {/* Aside en la parte lateral izquierda */}
+      <aside className="md:w-1/3 pl-4 mt-6 md:mt-0">
+        <Aside articles={articles} />
+      </aside>
+    </section>
+  );
 }
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import ArticleList from "./components/ArticleList";
 import { Article } from "./types/article";
-import { fetchData } from "../../lib/fetchUtils"; // Importa la función de utilidad
+import { fetchData } from "../../lib/fetchUtils";
 import Breadcrumb from "./components/Breadcrumb";
 
-// El componente ahora es asincrónico
 export default async function Blog() {
   try {
-    // Usamos la función de utilidad para obtener los artículos
     const articles: Article[] = await fetchData("/api/blog/articles");
 
     return (
-      <div className="flex flex-col md:flex-row relative ">
-        <div className="md:w-2/3 pr-4">
+      <div className="">
+        <div className="pr-4">
           <h1 className="text-3xl font-bold mb-6 text-yellow-300 pl-8">
             Artículos Recientes
           </h1>
