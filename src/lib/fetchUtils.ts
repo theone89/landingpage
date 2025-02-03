a6434cb87810610d1f1cdeee60e8a8dd9047c7b5
ThEOnE89 <franklincampos8907@gmail.com>
2025-02-02 23:12:05 -0500

    update

--- src/lib/fetchUtils.ts
+++ src/lib/fetchUtils.ts
@@ -2,8 +2,8 @@
 export async function fetchData(url: string) {
   const apiUrl =
     process.env.NODE_ENV === "production"
-      ? process.env.PRODUTION_API_URL
-      : process.env.lOCAL_API_URL;
+      ? "https://strongfreecode.com"
+      : "http://127.0.0.1:3000";
   try {
     const res = await fetch(`${apiUrl}${url}`);
 
