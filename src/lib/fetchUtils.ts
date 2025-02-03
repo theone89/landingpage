ec0ac59652f89e340a94ccf5d49589ec6e7c0cc1
ThEOnE89 <franklincampos8907@gmail.com>
2025-02-02 23:35:36 -0500

    update

--- src/lib/fetchUtils.ts
+++ src/lib/fetchUtils.ts
@@ -1,12 +1,8 @@
 // Fetch Utility para hacer solicitudes HTTP
 export async function fetchData(url: string) {
-  const apiUrl =
-    process.env.NODE_ENV === "production"
-      ? "https://strongfreecode.com"
-      : "http://127.0.0.1:3000";
   try {
-    const res = await fetch(`${apiUrl}${url}`);
-    console.log(apiUrl + url);
+    const res = await fetch(`${url}`);
+    console.log(url);
     if (!res.ok) {
       throw new Error(`Error en la solicitud: ${res.statusText}`);
     }
