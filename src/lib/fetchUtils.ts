4fe3abc206b3b65caab30ddf4dd16f5e121d109d
ThEOnE89 <franklincampos8907@gmail.com>
2025-02-02 23:42:19 -0500

    test

--- src/lib/fetchUtils.ts
+++ src/lib/fetchUtils.ts
@@ -1,8 +1,12 @@
 // Fetch Utility para hacer solicitudes HTTP
 export async function fetchData(url: string) {
+  const apiUrl =
+    process.env.NODE_ENV === "production"
+      ? process.env.NEXT_PUBLIC_SITE_URL
+      : "http://127.0.0.1:3000";
   try {
-    const res = await fetch(`${url}`);
-    console.log(url);
+    const res = await fetch(`${apiUrl}${url}`);
+    console.log(apiUrl + url);
     if (!res.ok) {
       throw new Error(`Error en la solicitud: ${res.statusText}`);
     }
